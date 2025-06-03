7a08f16d13e6c3ad87a5740865c6c31d86256727
Анастасия Кононова <anastasiakononova@MacBook-Air-Anastasia.local>
2025-06-03 14:30:50 +0300

    Add 2nd app test

--- index.js
+++ index.js
@@ -6,13 +6,17 @@ const PORT = process.env.NODE_ENV ?? 5000;
 const app = express();
 
 app.get('/', (req, res) => {
-    res.status(200).send('Ok');
-})
+  res.status(200).send('Ok');
+});
+
+app.get('/users/4', (req, res) => {
+  res.status(200).send({ name: 'Test' });
+});
 
 const server = http.createServer(app);
 
 server.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
 
-module.exports = app; 
+module.exports = app;
--- tests/app.test.js
+++ tests/app.test.js
@@ -2,7 +2,12 @@ const request = require('supertest');
 const app = require('./../index');
 
 describe('Testing app', () => {
-  it('response from GET / should be 200 "Ok"', done => {
+  it('response from GET / should be 200 "Ok"', (done) => {
     request(app).get('/').expect(200).expect('Ok').end(done);
+  });
+
+  // Протестувати GET /users/4 -> 200 {name: "Test"}Add commentMore actions
+  it('response from GET /users/4 expects 200 {name: "Test"}', (done) => {
+    request(app).get('/users/4').expect(200).expect({ name: 'Test' }).end(done);
   });
 });
